5ffba54ab88ef0e612dcce75fa74e09c264e5f0f
Zygimantas Domeikis <zygimantasdomeikis@yahoo.com>
2026-04-15 17:27:56 +0300

    Initial commit

---
+++ LetsTravelTests/LetsTravelTests.swift
@@ -0,0 +1,19 @@
+//
+//  LetsTravelTests.swift
+//  LetsTravelTests
+//
+//  Created by Zygimantas Domeikis on 15/04/2026.
+//
+
+import Testing
+@testable import LetsTravel
+
+struct LetsTravelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
