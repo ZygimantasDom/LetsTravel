7c51c5a033a9946e6168828445f5ea110e7b3408
Zygimantas Domeikis <zygimantasdomeikis@yahoo.com>
2026-04-16 21:37:46 +0300

    tab bar add

---
+++ LetsTravel/Screens/FavoritePlacesVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritePlacesVC.swift
+//  LetsTravel
+//
+//  Created by Zygimantas Domeikis on 16/04/2026.
+//
+
+import UIKit
+
+class FavoritePlacesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LetsTravel/Screens/TravelerMapVC.swift
@@ -0,0 +1,29 @@
+//
+//  TravelerMapVC.swift
+//  LetsTravel
+//
+//  Created by Zygimantas Domeikis on 16/04/2026.
+//
+
+import UIKit
+
+class TravelerMapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
